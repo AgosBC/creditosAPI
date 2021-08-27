4991a627dcb137fb65d44fb631c663871d3cb832
AgosBC <agostinabarello@gmail.com>
2021-08-27 18:30:51 -0300

    creacion del web method tipo post crearCliente, sujeto a cambios, faltan validaciones. creacion del generi response  y el cliente repository

--- src/main/java/ar/com/ada/api/creditos/controllers/ClienteController.java
+++ src/main/java/ar/com/ada/api/creditos/controllers/ClienteController.java
@@ -1,10 +1,36 @@
 package ar.com.ada.api.creditos.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import ar.com.ada.api.creditos.entities.Cliente;
+import ar.com.ada.api.creditos.models.response.GenericResponse;
+import ar.com.ada.api.creditos.services.ClienteService;
 
 @RestController
 public class ClienteController {
 
-    
+    @Autowired
+    ClienteService service;
+    @PostMapping
+    public ResponseEntity<GenericResponse> crearCliente(@RequestBody Cliente cliente){
+
+        GenericResponse r = new GenericResponse();
+
+        service.grabar(cliente);
+
+        r.isOk = true;
+        r.id = cliente.getClienteId();
+        r.mensaje = "el cliente fue creado con exito";
+
+
+        return ResponseEntity.ok(r);
+
+        
+
+    }
     
 }
--- src/main/java/ar/com/ada/api/creditos/services/ClienteService.java
+++ src/main/java/ar/com/ada/api/creditos/services/ClienteService.java
@@ -2,7 +2,18 @@ package ar.com.ada.api.creditos.services;
 
 import org.springframework.stereotype.Service;
 
+import ar.com.ada.api.creditos.entities.Cliente;
+import ar.com.ada.api.creditos.repos.ClienteRepository;
+
 @Service
 public class ClienteService {
+
+    ClienteRepository repo;
+
+    public void grabar(Cliente cliente) {
+        
+        repo.save(cliente);
+    
+    }
     
 }
